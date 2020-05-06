6ca27b6e31020073092e2fe98aa2a0e4a4aa982c
Stepan-Kolarovsky <60381824+Stepan-Kolarovsky@users.noreply.github.com>
2020-05-06 13:27:24 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Č1</h1>
+<?php
+
+    for ($ii=0; $ii <= 20; $ii+= 1) { 
+       echo $ii .", ";
+    }
+
+    ?>
+
+<p><h1>Č2</h1>
+    <?php
+
+    for ($i=0; $i <= 20; $i+= 1) { 
+       echo $i . " , " . $i*$i . "<br>";
+    }
+
+    ?> <p>
+
+<h1>Č3</h1>
+
+<?php
+
+for ($j=10; $j >= 0; $j-= 1) { 
+    echo $j ."<br>";
+ }
+
+    ?>
+Raketa letí
+
+<p><h1>Č4</h1>
+    <?php
+define('MAX', 100)
+define('MIN', 0)
+define('NUM', 10)
+    for ($k=0; $k <= 20; $k+= 1) { 
+       echo $k ."<br>";
+    }
+
+    ?> <p>
+
+</body>
+</html>
